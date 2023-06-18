63a9e6d48b9d34a404a28b3033e8c19357559d10
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2023-06-18 08:28:42 -0700

    Update psi_vis.py

--- maminian/psi_vis.py
+++ maminian/psi_vis.py
@@ -33,23 +33,15 @@ def plot_values(myax, values, cmap=plt.cm.viridis, vmin=None, vmax=None, boundar
     dp.set_array(values)
     myax.add_collection(dp)
     
-    #boundarycolor = 'k' if boundarycolor is None else boundarycolor
-    
-    # todo: linecollection
-    # TODO: ... pretty sure the sequence of data structure 
-    # ops are all ok ... ???
-    # Are we sure the algo identifying exterior faces is what it should be?
-
     if boundarycolor is not None:
-        for i in range(sy.geom.faces.num_interior, sy.geom.faces.num):
-            
-            #node_idxs = sy.geom.faces.nodes[:,i]
-            kk = sy.geom.faces.nodes[:,i]
-            #bdry = any( sy.geom.faces.neighbors[:,i] < 0 )
-            
-            xx,yy = sy.geom.nodes.coords[:, kk]
-            myax.plot( xx, yy, c=boundarycolor )
-    
+        from matplotlib.collections import LineCollection
+
+        kk = sy.geom.faces.nodes[:,range(sy.geom.faces.num_interior, sy.geom.faces.num)]
+        
+        tensor = np.transpose( sy.geom.nodes.coords[:, kk], [2,1,0] )
+        lc = LineCollection(tensor, color=boundarycolor)
+        myax.add_collection(lc)
+
     return dp
 
 ######
@@ -65,8 +57,10 @@ for i in range(ax.shape[0]):
     for j in range(ax.shape[1]):
         idx = j + i*ax.shape[1]
         que = plot_values( ax[i,j], sy.ALL_DATA['Psi_y'][:,idx], cmap=plt.cm.PRGn, vmin=vmn, vmax=vmx )
-        ax[i,j].text(0,0, r'$\psi_{%i}$'%(idx), transform=ax[i,j].transAxes)
-
+        ax[i,j].text(0.05,0.05, r'$\psi_{%i}$'%(idx), fontsize=18,  transform=ax[i,j].transAxes, bbox={'facecolor': 'w', 'edgecolor': 'k'})
+        
+        # fill background with hatching
+        ax[i,j].fill_between([0,1], [1,1], transform=ax[i,j].transAxes, hatch='////', edgecolor='#666', facecolor=[0,0,0,0], zorder=-100)
 
 fig.show()
 
